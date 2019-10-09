aa50e5f7e07274f3c43b15156f332256ebdb491e
Anthony Lopez-Aquino email = aquinoLopeza911@gmail.com username = aquinoLopez911 <anthonylopez-aquino@Anthonys-MacBook-Pro.local>
2019-10-09 16:30:05 -0400

    added docs

---
+++ docs/precache-manifest.ca5825aa1b9564f2f401f7da85133146.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac54d8b0cf4f726225c9cad1129d811a",
+    "url": "/dynamicVote/index.html"
+  },
+  {
+    "revision": "2acac47de0271ad2874a",
+    "url": "/dynamicVote/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "814860d1d2ce24e61ba0",
+    "url": "/dynamicVote/static/css/main.f51d4cc9.chunk.css"
+  },
+  {
+    "revision": "2acac47de0271ad2874a",
+    "url": "/dynamicVote/static/js/2.de89a260.chunk.js"
+  },
+  {
+    "revision": "814860d1d2ce24e61ba0",
+    "url": "/dynamicVote/static/js/main.096dc8f0.chunk.js"
+  },
+  {
+    "revision": "701af6f6c5f113ba1ead",
+    "url": "/dynamicVote/static/js/runtime-main.07b8ee6a.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/dynamicVote/static/media/logo.25bf045c.svg"
+  }
+]);
